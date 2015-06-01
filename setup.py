a878a5a60a803c6eb23e1294bc0aa944eac24007
Toby Oliver <toby@theolivers.org>
2015-06-01 23:28:27 +0200

    adding in initial files

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='PyRRC',
+      version='0.1',
+      description='Python Remote Router Control',
+      author='Toby Oliver',
+      author_email='toby@theolivers.org',
+      packages=['pyrrc'],
+     )
---
+++ tests/test_basic.py
@@ -0,0 +1,12 @@
+"""
+Some basic unit tests for communication with the router
+"""
+
+class TestLogin:
+    def test_login_fail(self):
+        # need to call paramiko to connect to router and fail
+        print "fail"
+
+    def test_login_pass(self):
+        # need to call paramiko to connect to router to pass
+	print "pass"
